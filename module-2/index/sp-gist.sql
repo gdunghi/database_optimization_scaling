0089705be1e09347f8e39cfbb56f9ef6e9187900
Jirayu Photisan <gdunghi@gmail.com>
2025-08-12 21:12:44 +0700

    remove invalid query section

--- module-2/index/sp-gist.sql
+++ module-2/index/sp-gist.sql
@@ -49,31 +49,3 @@ EXPLAIN ANALYSE SELECT * FROM ip_data
 WHERE ip << '192.168.0.0/16'
 LIMIT 10;
 
-
-
-EXPLAIN ANALYSE SELECT * FROM customers WHERE lower(name) = 'customer 999'; 
-
-
-
-WITH recent_orders AS (
-    SELECT order_id, customer_id, order_total
-    FROM customer_orders
-    WHERE order_date >= '2023-06-01'
-),
-high_value_recent_orders AS (
-    SELECT customer_id, SUM(order_total) AS total_spent
-    FROM recent_orders
-    GROUP BY customer_id
-    HAVING SUM(order_total) > 500
-)
-
-SELECT c.customer_name, hvo.total_spent
-FROM customer c
-JOIN (
-    SELECT customer_id, SUM(order_total) AS total_spent
-    FROM recent_orders
-    GROUP BY customer_id
-    HAVING SUM(order_total) > 500
-) hvo ON c.customer_id = hvo.customer_id;
-
-)
